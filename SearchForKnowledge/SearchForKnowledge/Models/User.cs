795ad063537a06c033a09083c4fb5242ec3d32b9
Viktor M <viktmedu@gmail.com>
2016-04-19 10:49:12 +0200

    Register Layout

---
+++ SearchForKnowledge/SearchForKnowledge/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchForKnowledge.Models
+{
+    public class User
+    {
+        private string username { get; set; }
+        private string password { get; set; }
+        private string schoolName { get; set; }
+        private string country { get; set; }
+        private string city { get; set; }
+
+        public User(string userName, string password, string schoolName, string country, string city)
+        {
+            this.username = username;
+            this.password = password;
+            this.schoolName = schoolName;
+            this.country = country;
+            this.city = city;
+        }
+    }
+}
